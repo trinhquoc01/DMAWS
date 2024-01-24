70f811928e3c1363b98f69c37c1a93ee8abc5cee
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2024-01-24 12:06:46 +0700

    Update Shopping.cs

--- DMAWS/Entities/Shopping.cs
+++ DMAWS/Entities/Shopping.cs
@@ -1,6 +1,31 @@
-﻿namespace DMAWS.Entities
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DMAWS.Entities
 {
-    public class Shopping
+    public class Order
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(64, MinimumLength = 3)]
+        public required string ItemCode { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength = 3)]
+        public required string ItemName { get; set; }
+
+        [Required]
+        public required int ItemQty { get; set; }
+
+        [Required]
+        public required DateTime OrderDelivery { get; set; }
+
+        [Required]
+        public required string OrderAddress { get; set; }
+
+        [Required]
+        public required string PhoneNumber { get; set; }
     }
 }
