37015302c9fd48cf862d6a4bbedee2bc75f0f5d7
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2024-01-24 12:07:56 +0700

    Update Program.cs

--- DMAWS/Program.cs
+++ DMAWS/Program.cs
@@ -1,27 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using DMAWS.DatabaseContexts;
+using DMAWS.Services.OrderService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connection db context
+builder.Services.AddDbContext<OrderContext>(optionsAction =>
+{
+    optionsAction.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Register services
+builder.Services.AddTransient<IOrderService, OrderService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
